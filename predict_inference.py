483e22804e87f6d551002e46d7294f68a9fc5b74
prhuppertz <prhuppertz@gmail.com>
2020-11-06 11:18:56 +0000

    inference script

--- predict_inference.py
+++ predict_inference.py
@@ -5,12 +5,12 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import os
 
 def get_args(parent_parser):
     parser = ArgumentParser(parents=[parent_parser], add_help=False)
     parser.add_argument('--checkpoint_path', type=str, help="adam: learning rate")
     return parser
-
 
 def main(args=None):
 
@@ -28,6 +28,9 @@ def main(args=None):
 
 if __name__ == "__main__":
     image_path = "C:/Users/Tobias/Downloads/HIDA-ufz_image_challenge/photos_annotated/2019_0626_080354_004.jpg.jpg"
+    file_index = image_path.index("/photos_annotated")
+    file_end_index = image_path.index(".jpg")
+    file_name = image_path[file_index:file_end_index]
     model = main()
     image = np.array(Image.open(image_path)) / 255
     image = np.moveaxis(image, -1, 0)
@@ -35,7 +38,12 @@ if __name__ == "__main__":
     input_tensor = torch.from_numpy(image).float()
 
     output = model(input_tensor).argmax(dim=1).detach().cpu().numpy()
+    
+    #copy to all channels
+    for i in range(3):
+        output[i]=output[0]
 
-    plt.imshow(output[0])
-    plt.show()
-    print("finished")
+    # save image as png
+    save_path="data/HIDA-ufz_image_challenge/photos_predictions"
+    im = Image.fromarray(output)
+    im.save(os.path.join(save_path, file_name + "_pred.jpg.png"))
