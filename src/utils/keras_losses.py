dcda276ef5537e0019680a7c0b9ab6ca0316682f
SRSteinkamp <simon.steinkamp@googlemail.com>
2020-11-06 11:26:01 +0100

    moved loss outside, clean up

--- src/utils/keras_losses.py
+++ src/utils/keras_losses.py
@@ -59,3 +59,30 @@ def FocalTverskyLoss(targets, inputs, alpha=0.5, beta=0.5, gamma=1, smooth=1e-6)
         FocalTversky = K.pow((1 - Tversky), gamma)
         
         return FocalTversky
+    
+    
+def dice_coef_binary(y_true, y_pred, smooth=1e-7):
+    '''
+    Dice coefficient for 2 categories. Ignores background pixel label 0
+    Pass to model as metric during compile statement
+    '''
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersect = K.sum(y_true_f * y_pred_f, axis=-1)
+    denom = K.sum(y_true_f + y_pred_f, axis=-1)
+    return K.mean((2. * intersect / (denom + smooth)))
+
+
+def dice_coef_binary_loss(y_true, y_pred):
+    '''
+    Dice loss to minimize. Pass to model as loss during compile statement
+    '''
+    return 1 - dice_coef_binary(y_true, y_pred)
+
+
+# Add binary cross entropy
+def BCE_dice(y_true, y_pred):
+    cce = K.categorical_crossentropy(y_true[:, :, :], y_pred[:, :, :])
+    dice = Loss.dice_loss(y_true[:, :, :], y_pred[:, :, :])
+    dice2 = dice_coef_binary_loss(y_true[:, :, 1:], y_pred[:, :, 1:])
+    return  0.33 * dice + 0.33 * cce  + 0.33 * dice2
